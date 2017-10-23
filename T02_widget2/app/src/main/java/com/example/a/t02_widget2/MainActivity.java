d5d2dcf2064aa59d995e3f68217fb61537aedf98
Your Name <you@example.com>
2017-10-23 17:15:06 +0900

    라디오버튼

--- T02_widget2/app/src/main/java/com/example/a/t02_widget2/MainActivity.java
+++ T02_widget2/app/src/main/java/com/example/a/t02_widget2/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.a.t02_widget2;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "가나다라", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RadioGroup myRadioGroup = (RadioGroup) findViewById(R.id.myRadioGroup);
+        //라디오 그룹 라디오 버튼 체크가 변경됬을때 이벤트 발생
+        myRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int i) {
+                switch (i){
+                    case R.id.myRadio1 :
+                        Toast.makeText(MainActivity.this, "라디오1", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.myRadio2 :
+                        Toast.makeText(MainActivity.this, "라디오2", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.myRadio3 :
+                        Toast.makeText(MainActivity.this, "라디오3", Toast.LENGTH_SHORT).show();
+                        break;
+                }
             }
         });
     }
