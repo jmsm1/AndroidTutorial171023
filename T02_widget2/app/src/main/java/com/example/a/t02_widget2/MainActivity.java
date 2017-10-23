7269dca6e6d90402fc78096adf92796f9d1b0019
Your Name <you@example.com>
2017-10-23 16:57:30 +0900

    클립이벤트 추가

--- T02_widget2/app/src/main/java/com/example/a/t02_widget2/MainActivity.java
+++ T02_widget2/app/src/main/java/com/example/a/t02_widget2/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.a.t02_widget2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn = (Button) findViewById(R.id.textViewId1);
-        btn.setOnClickListener(new View.OnClickListener(){
-
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "가나다라", Toast.LENGTH_SHORT).show();
+            }
         });
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-
     }
 }
