12d0dde2c54c5027ddef7a27c80200382db51644
Your Name <you@example.com>
2017-10-25 17:40:46 +0900

    서비스

---
+++ T15_service/app/src/main/java/com/example/a/t15_service/MyIntentService.java
@@ -0,0 +1,23 @@
+package com.example.a.t15_service;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions and extra parameters.
+ */
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
