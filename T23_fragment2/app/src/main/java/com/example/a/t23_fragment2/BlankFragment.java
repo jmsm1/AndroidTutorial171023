3635f276ef3768cea2d896acb8c2101984aca3ac
Your Name <you@example.com>
2017-10-27 12:14:29 +0900

    fragment2

---
+++ T23_fragment2/app/src/main/java/com/example/a/t23_fragment2/BlankFragment.java
@@ -0,0 +1,43 @@
+package com.example.a.t23_fragment2;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+
+    public BlankFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_blank, container, false);
+        final TextView textViewCount = v.findViewById(R.id.textViewCounter);
+        Button btnIncrease = v.findViewById(R.id.btnIncrease);
+
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int number = Integer.parseInt(textViewCount.getText().toString());
+                number++;
+                textViewCount.setText(""+number);
+            }
+        });
+        return v;
+    }
+
+}
--- T23_fragment2/app/src/main/java/com/example/a/t23_fragment2/MainActivity.java
+++ T23_fragment2/app/src/main/java/com/example/a/t23_fragment2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.a.t23_fragment2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClick(View v){
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fr = manager.findFragmentById(R.id.frame);
+        switch (v.getId()){
+            case R.id.btnAdd :
+                if(fr == null){
+                    fr = new BlankFragment();
+                    FragmentTransaction tr = manager.beginTransaction();
+                    tr.add(R.id.frame , fr , "counter");
+                    tr.commit();
+                }
+                break;
+            case R.id.btnRemove :
+                if(fr != null){
+                    FragmentTransaction tr = manager.beginTransaction();
+                    tr.remove(fr);
+                    tr.commit();
+                }
+
+                break;
+            case R.id.btnReplace :
+                if(fr != null){
+                    FragmentTransaction tr = manager.beginTransaction();
+                    if(fr.getTag().equals("counter")){
+                        TextFragment tf = new TextFragment();
+                        tr.replace(R.id.frame,tf,"text");
+                        tr.commit();
+                    }else{
+                        BlankFragment bf = new BlankFragment();
+                        tr.replace(R.id.frame, bf,"counter");
+                        tr.commit();
+                    }
+                }
+
+                break;
+
+            case R.id.btnHide :
+                if(fr != null){
+                    FragmentTransaction tr = manager.beginTransaction();
+                    if(fr.isHidden()){
+                        tr.show(fr);
+                    }else{
+                        tr.hide(fr);
+                    }
+                    tr.commit();
+                }
+                break;
+        }
     }
 }
